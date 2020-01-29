b079298b0357b2d52446d947aecc286a0a477e63
Dmitrij Kosmakov <digital.artificial@gmail.com>
2020-01-29 23:47:14 +0200

    Add DS/Lists/ReverseLinkedList (Py)

---
+++ Data Structures/Linked Lists/reverse_linked_list_test.py
@@ -0,0 +1,26 @@
+from reverse_linked_list import SinglyLinkedList, SinglyLinkedListNode, reverse, print_singly_linked_list
+
+def __getSLLElements(node: SinglyLinkedListNode):
+    while node:
+        yield node.data
+        node = node.next
+
+def __validate(expected: list, sll: SinglyLinkedList):
+    assert expected == list(__getSLLElements( reverse(sll.head) ))
+
+def test_sample_input_0():
+    sll = SinglyLinkedList()
+    sll.insert_node(1)
+    sll.insert_node(2)
+    sll.insert_node(3)
+    sll.insert_node(4)
+    sll.insert_node(5)
+    __validate([5,4,3,2,1], sll)
+    
+def test_sample_input_1():
+    sll = SinglyLinkedList()
+    sll.insert_node(3)
+    sll.insert_node(4)
+    sll.insert_node(2)
+    sll.insert_node(5)
+    __validate([5,2,4,3], sll)
--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 ### Arrays
 #### Array Manipulation (Python3)
 https://www.hackerrank.com/challenges/crush/submissions/code/140352475
+### Linked Lists
+#### Reverse a linked list
+https://www.hackerrank.com/challenges/reverse-a-linked-list/submissions/code/140361083
 ### Bits
 #### Flipping bits (Python3)
 https://www.hackerrank.com/challenges/flipping-bits/submissions/code/140346706
