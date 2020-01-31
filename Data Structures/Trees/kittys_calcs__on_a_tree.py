fc5cf927b8ea8a8377fa66cca89647afcd95224e
Dmitrij Kosmakov <digital.artificial@gmail.com>
2020-02-01 00:59:51 +0200

    2. __WIP__

--- Data Structures/Trees/kittys_calcs__on_a_tree.py
+++ Data Structures/Trees/kittys_calcs__on_a_tree.py
@@ -1,6 +1,4 @@
-# import sys
-from collections import deque
-from itertools import combinations
+import sys
 from functools import reduce
 
 MOD = 10**9+7
@@ -8,32 +6,30 @@ MOD = 10**9+7
 
 # function for finding minimum
 # no. of edge using BFS
-def minEdgeBFS(edges: list, u: int, v: int):
+def minEdgeBFS(edges: list, dist: list, visit: list, u: int, v: int):
+    from collections import deque
     n = len(edges)
 
-    # visited[n] for keeping track
-    # of visited node in BFS
-    visited = [0] * n
-    # Initialize distances as 0
-    distance = [0] * n
+    # visited[n] for keeping track of visited node in BFS
+    visited = visit[u]
+    distance = dist[u]
     # queue to do BFS.
     deq = deque()
     distance[u] = 0
 
-    # deq.put(u)
     deq.append(u)
-
     visited[u] = True
     while deq:
         x = deq.pop()
         for i in range(len(edges[x])):
-            if visited[edges[x][i]]:
+            if edges[x][i] in visited:
                 continue
             # update distance for i
             distance[edges[x][i]] = distance[x] + 1
             deq.append(edges[x][i])
-            visited[edges[x][i]] = 1
+            visited[edges[x][i]] = True
     return distance[v]
+
 
 """class __Mock_STDIN(object):
     def __init__(self, response_list):
@@ -49,6 +45,7 @@ def minEdgeBFS(edges: list, u: int, v: int):
         else:
             self.index = 0
         return value
+
 
 predetermined_stdin_responses = [
     '7 3\r',
@@ -68,6 +65,12 @@ predetermined_stdin_responses = [
 sys.stdin = __Mock_STDIN(predetermined_stdin_responses)"""
 
 
+def distances(n: int):
+    d = [{} for i in range(n)]
+    v = d.copy()
+    return d, v
+
+
 def toAdjacencyList(n: int, edges):
     adj = [[] for i in range(n)]
     for edge in edges:
@@ -78,25 +81,26 @@ def toAdjacencyList(n: int, edges):
     return adj
 
 
-def solveSingle(adj: list, u: int, v: int):
-    dist = minEdgeBFS(adj, u-1, v-1)
+def solve(adj: list, dist: list, visit: list, u: int, v: int):
+    d = minEdgeBFS(adj, u-1, v-1, dist, visit)
     global MOD
-    return dist * u * v % MOD
+    return d * u * v % MOD
 
 
-def solveMultiple(adj: list, iterable):
+def solveMultiple(adj: list, dist: list, visit: list, iterable):
+    from itertools import combinations
+
     c = combinations(iterable, 2)
-    r = reduce(lambda total, x: total + solveSingle(adj, x[0], x[1]), c, 0)
+
+    def red(total: int, x):
+        return total + solve(adj, dist, visit, x[0], x[1])
+
+    r = reduce(red, c, 0)
     global MOD
     return r % MOD
 
 
-def solveMultipleSets(adj: list, s):
-    for q in s:
-        yield solveMultiple(adj, q)
-
-
-if __name__ == '__main__':
+def processInput():
     nq = list(map(int, input().split()))
     n = nq[0]
     q = nq[1]
@@ -109,7 +113,13 @@ if __name__ == '__main__':
     for idx in range(q):
         _ = input()
         sets.append(set(map(int, input().split())))
+    return n, edges, sets
 
+
+
+if __name__ == '__main__':
+    n, edges, sets = processInput()
     adj = toAdjacencyList(n, edges)
-    for s in solveMultipleSets(adj, sets):
-        print(s)
+    dist, visit = distances(n)
+    for s in sets:
+        print(solveMultiple(adj, dist, visit, s))
--- Data Structures/Trees/kittys_calcs__on_a_tree_test.py
+++ Data Structures/Trees/kittys_calcs__on_a_tree_test.py
@@ -1,10 +1,40 @@
-from kittys_calcs__on_a_tree import toAdjacencyList, solveMultipleSets
+from kittys_calcs__on_a_tree import distances, toAdjacencyList, \
+                                    solve, solveMultiple
+
+
+"""def __validate_distances(distances):
+    n = len(distances)
+    assert all(map(lambda x: len(x) == n, distances))
+    for i, d in enumerate(distances):
+        for j, v in enumerate(d):
+            assert v == 0 if i == j else v != 0
+
+
+def test_distances():
+    assert distances(0) == []
+    assert distances(1) == [[0]]
+    assert distances(2) == [[0, -1],
+                            [-1, 0]]
+    assert distances(5) == [[0, -1, -1, -1, -1],
+                            [-1, 0, -1, -1, -1],
+                            [-1, -1, 0, -1, -1],
+                            [-1, -1, -1, 0, -1],
+                            [-1, -1, -1, -1, 0]]
+
+    __validate_distances(distances(2*(10**5)))"""
 
 
 def test_sample_input_0():
     edges = [[1, 2], [1, 3], [1, 4], [3, 5], [3, 6], [3, 7]]
-    sets = [[2, 4], [5], [2, 4, 5]]
+    sets = [[2, 4], [5], [2, 4, 5], [4, 5]]
+    n = len(edges) + 1
 
-    adj = toAdjacencyList(len(edges)+1, edges)
-    result = list(solveMultipleSets(adj, sets))
-    assert result == [16, 0, 106]
+    adj = toAdjacencyList(n, edges)
+    dist, visit = distances(n)
+
+    def solveMultipleSets(adj, dist, visited, sets):
+        for s in sets:
+            yield solveMultiple(adj, dist, visited, s)
+
+    result = list(solveMultipleSets(adj, dist, visit, sets))
+    assert result == [16, 0, 106, 60]
